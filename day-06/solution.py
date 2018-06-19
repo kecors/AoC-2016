095ec1d459a68fee1a9070badb012b1136755037
Kenneth Collins <dev@kecors.net>
2018-06-19 12:44:34 -0400

    Solve day 6 part 2

--- day-06/solution.py
+++ day-06/solution.py
@@ -5,7 +5,8 @@ class State:
         self.positions = []
         for x in range(8):
             self.positions.append([])
-        self.corrected_message = ''
+        self.p1_corrected_message = ''
+        self.p2_corrected_message = ''
 
     def populate(self, message):
         for x in range(8):
@@ -14,8 +15,9 @@ class State:
     def evaluate(self):
         for x in range(8):
             counter = collections.Counter(self.positions[x])
-            most_common = counter.most_common(1)
-            self.corrected_message += most_common[0][0]
+            most_common = counter.most_common()
+            self.p1_corrected_message += most_common[0][0]
+            self.p2_corrected_message += most_common[-1][0]
 
 def main():
     state = State()
@@ -23,7 +25,8 @@ def main():
     for line in file:
         state.populate(line.strip())
     state.evaluate()
-    print("Part 1: the error-corrected version of the message is {}".format(state.corrected_message))
+    print("Part 1: the error-corrected version of the message is {}".format(state.p1_corrected_message))
+    print("Part 2: the error-corrected version of the message is {}".format(state.p2_corrected_message))
 
 if __name__ == "__main__":
     main()
