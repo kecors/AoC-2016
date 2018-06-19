c86108aeb408ec89bcfb70c27cfaf39b492bff82
Kenneth Collins <dev@kecors.net>
2018-06-18 20:40:13 -0400

    Solve day 6 part 1

---
+++ day-06/solution.py
@@ -0,0 +1,29 @@
+import collections
+
+class State:
+    def __init__(self):
+        self.positions = []
+        for x in range(8):
+            self.positions.append([])
+        self.corrected_message = ''
+
+    def populate(self, message):
+        for x in range(8):
+            self.positions[x].append(message[x])
+
+    def evaluate(self):
+        for x in range(8):
+            counter = collections.Counter(self.positions[x])
+            most_common = counter.most_common(1)
+            self.corrected_message += most_common[0][0]
+
+def main():
+    state = State()
+    file = open("puzzle-input.txt", "r")
+    for line in file:
+        state.populate(line.strip())
+    state.evaluate()
+    print("Part 1: the error-corrected version of the message is {}".format(state.corrected_message))
+
+if __name__ == "__main__":
+    main()
