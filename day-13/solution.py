d77b50c1b53e15908af97f071a269fc4bab0626c
Kenneth Collins <dev@kecors.net>
2018-06-25 19:54:17 -0400

    Solve day 13 part 2

--- day-13/solution.py
+++ day-13/solution.py
@@ -1,14 +1,14 @@
-import heapq
-
 class State:
     FAVORITE = 1350
     WIDTH = 60
     HEIGHT = 60
     ORIGIN = (1, 1)
     TARGET = (31, 39)
+    DELTAS = [[0,1], [1,0], [0,-1], [-1,0]]
 
     def __init__(self):
         self.grid = [False] * State.WIDTH * State.HEIGHT
+        self.parent = {}
 
     def is_wall(self, x, y):
         result = x*x + 3*x + 2*x*y + y + y*y + State.FAVORITE
@@ -28,53 +28,64 @@ class State:
             output = ""
             for x in range(State.WIDTH):
                 if x == State.ORIGIN[0] and y == State.ORIGIN[1]:
-                    output += 'B'
-                    continue
-                if x == State.TARGET[0] and y == State.TARGET[1]:
                     output += 'O'
                     continue
+                if x == State.TARGET[0] and y == State.TARGET[1]:
+                    output += 'T'
+                    continue
                 if self.grid[y * State.WIDTH + x] == True:
-                    output += 'X'
+                    output += '#'
                 else:
                     output += '.'
-            #print output
+            print output
 
-    def distance(self, x, y):
-        return abs(State.TARGET[0] - x) + abs(State.TARGET[1] - y)
+    def distance_to_origin(self, x, y):
+        sum = 0
+        while (x, y) != State.ORIGIN:
+            (x, y) = self.parent[(x, y)]
+            sum += 1
+        return sum
 
     def traverse(self):
-        deltas = [[0,1], [1,0], [0,-1], [-1,0]]
-        heap = []
-        parents = {}
-        (x, y) = State.ORIGIN
-        distance = self.distance(x, y)
-        heapq.heappush(heap, (distance, x, y))
+        self.parent[State.ORIGIN] = State.ORIGIN
+        stack = [State.ORIGIN]
         while True:
-            (distance, x, y) = heapq.heappop(heap)
-            (target_x, target_y) = State.TARGET
-            if x == target_x and y == target_y:
-                path = []
-                while True:
-                    if (x, y) == State.ORIGIN:
-                        return len(path)
-                    if (x, y) in parents:
-                        path.append((x, y))
-                        (x, y) = parents[(x, y)]
-            for (dx, dy)  in deltas:
+            if len(stack) == 0:
+                break
+            (x, y) = stack.pop()
+            dist = self.distance_to_origin(x, y)
+            for (dx, dy) in State.DELTAS:
                 new_x, new_y = x + dx, y + dy
-                if (new_x, new_y) in parents:
+                if new_x < 0 or new_y < 0:
                     continue
-                if self.grid[new_y * State.WIDTH + new_x] == False:
-                    parents[(new_x, new_y)] = (x, y)
-                    new_distance = self.distance(new_x, new_y)
-                    heapq.heappush(heap, (new_distance, new_x, new_y))
+                if self.grid[new_y * State.WIDTH + new_x] == True:
+                    continue
+                if (new_x, new_y) in self.parent:
+                    (px, py) = self.parent[(new_x, new_y)]
+                    if dist < self.distance_to_origin(px, py):
+                        self.parent[(new_x, new_y)] = (x, y)
+                        stack.append((new_x, new_y))
+                else:
+                    self.parent[(new_x, new_y)] = (x, y)
+                    stack.append((new_x, new_y))
+
+    def at_most_50_steps(self):
+        sum = 0
+        for (x, y) in self.parent:
+            if self.distance_to_origin(x, y) <= 50:
+                sum += 1
+        return sum
 
 def main():
     state = State()
     state.build_grid()
-    state.display_grid()
-    result = state.traverse()
-    print("Part 1: the fewest number of steps required to reach (31,39) is {}".format(result))
+    #state.display_grid()
+    state.traverse()
+    (tx, ty) = state.TARGET
+    p1_result = state.distance_to_origin(tx, ty)
+    p2_result = state.at_most_50_steps()
+    print("Part 1: the fewest number of steps required to reach (31,39) is {}".format(p1_result))
+    print("Part 2: {} locations can be reached in at most 50 steps".format(p2_result))
 
 if __name__ == "__main__":
     main()
