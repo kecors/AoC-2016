7290192ffd5dc10c0e0ca0d97f685c83ee8daa8d
Kenneth Collins <dev@kecors.net>
2018-06-27 16:58:11 -0400

    Solve day 14 part 2

--- day-14/solution.py
+++ day-14/solution.py
@@ -1,42 +1,59 @@
 import hashlib
 import re
+from collections import deque
 
-SALT = "cuanljph"
+class State:
+    SALT = "cuanljph"
 
-def find_three(start):
-    index = start
-    while True:
+    def __init__(self, key_stretching_flag):
+        self.hashes = deque()
+        self.keys = []
+        self.key_stretching_flag = key_stretching_flag
+
+    def generate_hash(self, index):
         m = hashlib.md5()
-        m.update("{}{}".format(SALT, index))
+        m.update("{}{}".format(State.SALT, index))
         digest = m.hexdigest()
-        match = re.search(r'(.)\1\1', digest)
-        if match:
-            return(index, match.group(1))
-        index += 1
+        if self.key_stretching_flag == True:
+            for _ in range(2016):
+                m = hashlib.md5()
+                m.update(digest)
+                digest = m.hexdigest()
+        self.hashes.append(digest)
 
-def find_five(start, char):
-    index = start
-    while index <= (start + 1000):
-        md5 = hashlib.md5()
-        md5.update("{}{}".format(SALT, index))
-        digest = md5.hexdigest()
-        match = re.search(r'(%s)\1\1\1\1' % char, digest)
-        if match:
-            return True
-        index += 1
-    return False
+    def compute(self):
+        index = 0
+        while True:
+            if len(self.hashes) == 0:
+                self.generate_hash(index)
+            hash3 = self.hashes.popleft()
+            match = re.search(r'(.)\1\1', hash3)
+            if match:
+                char = match.group(1)
+                j = 0
+                while j < 1000:
+                    if len(self.hashes) <= j:
+                        self.generate_hash(index + 1 + j)
+                    hash5 = self.hashes[j]
+                    match = re.search(r'(%s)\1\1\1\1' % char, hash5)
+                    if match:
+                        self.keys.append((index, hash3))
+                        break
+                    j += 1
+
+            if len(self.keys) >= 64:
+                (index, hash3) = self.keys.pop()
+                return index
+
+            index += 1
 
 def main():
-    keys = 0
-    j = -1
-    while keys < 64:
-        (j, c) = find_three(j+1)
-        z = find_five(j+1, c)
-        if z == True:
-            keys += 1
-
-    p1_result = j
+    state = State(False)
+    p1_result = state.compute()
     print("Part 1: index {} produces the 64th one-time pad key".format(p1_result))
+    state = State(True)
+    p2_result = state.compute()
+    print("Part 2: index {} now produces the 64th one-time pad key".format(p2_result))
 
 if __name__ == "__main__":
     main()
